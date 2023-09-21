05dcce41392f6205c4c62e22d5fa7d65c2887f7a
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-09-21 14:11:17 +0900

    [Bronze I] Title: 세로읽기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,13 +1,6 @@
-li = []
-le = 0
-result = ""
-for i in range(5):
-    a = input()
-    li.append(list(a))
-    if len(a)>=le:
-        le=len(a)
-for j in range(le):
-    for i in li:
-        if len(i)-1>=j:
-            result+= i[j]
-print(result)
+words = [input() for i in range(5)]
+
+for i in range(15):
+    for j in range(5):
+        if i < len(words[j]):
+            print(words[j][i], end="")

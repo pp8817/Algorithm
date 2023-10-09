dee461807ac323b6d7948b31fb42b80a9d4dfcd9
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-10-09 21:46:48 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 868 ms, Memory: 119780 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -2,15 +2,15 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 card = list(map(int, input().split()))
-s = set(list(card))
-dic = {}
-for  i in s:
-    dic[i] = 0
-for i in card:
-    dic[i] +=1
-
 m = int(input())
 check = list(map(int, input().split()))
+dic = {}
+for i in card:
+    if i in dic:
+        dic[i] += 1
+    else:
+        dic[i] = 1
+    
 for j in check:
     if j in dic:
         print(dic[j], end=' ')

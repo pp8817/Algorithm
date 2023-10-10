b688565026e80070df92059d03067880cb75c6cb
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-10-11 01:23:14 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 612 ms, Memory: 240712 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -2,9 +2,7 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 s = input()
 arr = set()
-for i in s:
-    arr.add(i)
 for i in range(len(s)):
-    for j in range(i+1, len(s)):
-        arr.add(s[i]+s[i+1:j+1])
+    for j in range(i, len(s)):
+        arr.add(s[i:j+1])
 print(len(arr))

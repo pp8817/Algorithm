e1efc41e80dd0a64282a4bf6844bfa9c4fb45edd
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-10-11 22:47:27 +0900

    [Silver III] Title: 분수 합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.py
@@ -0,0 +1,14 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+def gcd(x,y): #최대공약수, 유클리드 호제
+    mod = x % y
+    while mod >0:
+        x = y
+        y = mod
+        mod = x % y
+    return y
+
+A, B = map(int, input().split())
+C, D = map(int, input().split())
+N = gcd(A*D + C*B, B*D) 
+print((A*D + C*B)//N, B*D//N)

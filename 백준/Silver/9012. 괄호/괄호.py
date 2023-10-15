4342721b6f1147e91746203a5b639e4b28ffdb42
Psm_01 <71458064+pp8817@users.noreply.github.com>
2023-10-15 22:26:39 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -3,16 +3,14 @@ input = lambda: sys.stdin.readline().rstrip()
 
 def check_parens(PS):
     stack = []
-    op='({['
-    dic = {')':'(', '}':'{', ']':'['}
 
     for c in PS:
-        if c in op:
+        if c == '(':
             stack.append(c)
         else:
             if len(stack) == 0:
                 return "NO"
-            elif dic[c] != stack[-1]:
+            elif "(" != stack[-1]:
                 return "NO"
             else:
                 stack.pop()

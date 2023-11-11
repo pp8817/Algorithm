f1300b9a439336d3c41a32561074ea74fe94a541
박상민 <71458064+pp8817@users.noreply.github.com>
2023-11-11 17:57:37 +0900

    [level 1] Title: 기사단원의 무기, Time: 10.22 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,17 @@
+def cf(n): # 공약수 출력
+    a = []
+    for i in range(1,int(n**0.5)+1):
+        if n%i == 0:
+            a.append(n//i)
+            a.append(i)
+    return len(set(a))
+
+def solution(number, limit, power):
+    answer=0
+    for i in range(1, number+1):
+        div_num=cf(i)
+        if div_num <= limit:
+            answer+=div_num
+        else:
+            answer+=power
+    return sum([cf(i) if cf(i)<=limit else power for i in range(1,number+1)])

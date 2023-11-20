8fabb4a75591b688c8c9888e48d4a343f1c3616e
박상민 <71458064+pp8817@users.noreply.github.com>
2023-11-20 11:05:11 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -2,9 +2,6 @@ def solution(food):
     answer=''
     food = food[1:]
     for i, c in enumerate(food):
-        c = c//2
-        
-        if c>=1:
-            answer+=str(i+1)*c
+        answer+=str(i+1)*(c//2)
     answer = answer+'0'+answer[::-1]
     return answer

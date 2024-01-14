bc67337c38b2f282f31f883e4f177455f6b1a44b
박상민 <pp8817@naver.com>
2024-01-15 03:23:51 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 76 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,53 +1,36 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
-	
-	static int N;
-	static int M;
-	static int[] arr;
-	
-	public static void main(String[] args) throws Exception {
-		
-		st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N+1];
-		
-		for(int i=1;i<=N;++i) {
-			arr[i] = i;
-		}
-		
-		for(int i=0;i<M;++i) {
-			st = new StringTokenizer(br.readLine());
-			
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			
-			reverse(s,e);
-		}
-		
-		for(int i=1;i<=N;++i) {
-			sb.append(arr[i] + " ");
-		}
-		System.out.println(sb);
-	}
-	
-	static void reverse(int start,int end) {
-		
-		for(int i=0;i<=(end-start)/2;++i) {
-			int temp = arr[start+i];
-			arr[start+i] = arr[end-i];
-			arr[end-i] = temp;
-		}
-	}
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = br.readLine().split(" ");
+        int N = Integer.parseInt(str[0]);
+        int M = Integer.parseInt(str[1]);
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int i = 0; i < M; i++) {
+            String[] str1 = br.readLine().split(" ");
+            int left = Integer.parseInt(str1[0])-1;
+            int right = Integer.parseInt(str1[1])-1;
+
+            while (left < right) {
+                int temp = arr[left];
+                arr[left++] = arr[right];
+                arr[right--] = temp;
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
 }

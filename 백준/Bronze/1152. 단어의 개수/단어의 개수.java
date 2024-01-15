f52a3cf1408f01e90be0e64367ff0d0e5b3c1598
박상민 <pp8817@naver.com>
2024-01-15 23:12:06 +0900

    [Bronze II] Title: 단어의 개수, Time: 188 ms, Memory: 20804 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine(), " ");
+        
+        System.out.print(st.countTokens());
+    }
+}

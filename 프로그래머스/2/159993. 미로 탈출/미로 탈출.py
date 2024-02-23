3fc063c69b362e6a93547726859f54a7bfc0c3f5
박상민 <pp8817@naver.com>
2024-02-23 13:53:34 +0900

    [level 2] Title: 미로 탈출, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -16,35 +16,35 @@ def solution(maps):
                 ex, ey = i, j
             elif maps[i][j] == "L":
                 lx, ly = i, j
+                
+    def bfs(x, y, point_x, point_y):
+        d = [[0,1], [0,-1], [1,0], [-1,0]]
+        visited = [[False]*H for _ in range(W)]
+
+        q = deque([(x,y,0)])
+
+        while q:
+            r, c, k = q.popleft()
+
+            if (r, c) == (point_x, point_y):
+                return k
+
+            for dx, dy in d:
+                nx, ny = r+dx, c+dy
+
+                if 0<=nx<W and 0<=ny<H:
+                    if maps[nx][ny] != "X" and not visited[nx][ny]:
+                        visited[nx][ny] = True
+                        q.append((nx, ny, k+1))
+        return -1
     
-    K1 = bfs(x, y, lx, ly, maps, W, H)
+    K1 = bfs(x, y, lx, ly)
     if K1 == -1:
         return -1
     answer += K1
-    K2 = bfs(lx, ly, ex, ey, maps, W, H)
+    K2 = bfs(lx, ly, ex, ey)
     if K2 == -1:
         return -1
     answer += K2
 
     return answer
-
-def bfs(x, y, point_x, point_y, maps, W, H):
-    d = [[0,1], [0,-1], [1,0], [-1,0]]
-    visited = [[False]*H for _ in range(W)]
-    
-    q = deque([(x,y,0)])
-    
-    while q:
-        r, c, k = q.popleft()
-        
-        if (r, c) == (point_x, point_y):
-            return k
-        
-        for dx, dy in d:
-            nx, ny = r+dx, c+dy
-            
-            if 0<=nx<W and 0<=ny<H:
-                if maps[nx][ny] != "X" and not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    q.append((nx, ny, k+1))
-    return -1

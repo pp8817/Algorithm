57e44979b1cb20b8c9d09077f2c45e2dedd193f8
박상민 <pp8817@naver.com>
2024-03-11 14:28:58 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -3,9 +3,10 @@ input = lambda: sys.stdin.readline().rstrip()
 
 L = int(input())
 s = input()
+M = 1234567891
 
 answer = 0
 for i in range(L):
     st = s[i]
     answer += (ord(st)-96)*(31**i)
-print(answer)
+print(answer%M)

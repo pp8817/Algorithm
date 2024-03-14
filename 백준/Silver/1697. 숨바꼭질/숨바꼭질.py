e29be0372e7bd4a22ca3aef351359bc1babc1335
박상민 <pp8817@naver.com>
2024-03-14 13:04:19 +0900

    [Silver I] Title: 숨바꼭질, Time: 144 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+input = lambda: sys.stdin.readline().rstrip()
+
+N, K = map(int, input().split())
+e = 100001
+visited = [False]*e
+
+q = deque()
+q.append((N, 0))
+visited[N] = 1
+
+while q:
+    x, c = q.popleft()
+    if x == K:
+        print(c)
+        break
+    for i in [1, -1]:
+        nx = x + i
+        if 0<= nx < e and not visited[nx]:
+            visited[nx] = visited[x]+1
+            q.append((nx, c+1))
+    if 0<= 2*x < e and not visited[2*x]:
+        visited[2*x] = visited[x]+1
+        q.append((2*x, c+1))

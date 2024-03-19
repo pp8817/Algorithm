7733f82987c7aba97a1a903914b984dee3c515d3
박상민 <pp8817@naver.com>
2024-03-19 16:02:50 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,14 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+h = int(input())
+stair = [0] + [int(input()) for i in range(1, h + 1)]
+if h < 2: 
+	print(stair[h])
+else:
+	dp = [0] * (h + 1)
+	dp[1] = stair[1]
+	dp[2] = dp[1] + stair[2]
+	for i in range(3, h + 1):
+		dp[i] = max(dp[i - 2], dp[i - 3] + stair[i - 1]) + stair[i]
+	print(dp[h])

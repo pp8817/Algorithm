e20ede6df0a30662b33fe93c8c3979d06a8d76db
박상민 <pp8817@naver.com>
2024-05-19 22:08:17 +0900

    [Gold IV] Title: 거짓말, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -0,0 +1,49 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def find(parent, x):
+    if x != parent[x]:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b, know_truth):
+    a = find(parent, a)
+    b = find(parent, b)
+
+    if a in know_truth and b in know_truth:
+        return
+
+    if a in know_truth:
+        parent[b] = a
+    elif b in know_truth:
+        parent[a] = b
+    else:
+        if a < b:
+            parent[b] = a
+        else:
+            parent[a] = b
+
+n, m = map(int, input().split())
+know_truth = list(map(int, input().split()))[1:]
+
+parties = []
+parent = list(range(n+1))
+
+for _ in range(m):
+    a = list(map(int, input().split()))
+    party_len = a[0]
+    party = a[1:]
+
+    for i in range(party_len-1):
+        union(parent, party[i], party[i+1], know_truth)
+    
+    parties.append(party)
+
+ans = 0
+for party in parties:
+    for i in range(len(party)):
+        if find(parent, party[i]) in know_truth:
+            break
+    else:
+        ans += 1
+print(ans)

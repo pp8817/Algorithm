de4f0d5b549ba99a16813257fd8738c32d7c656e
박상민 <pp8817@naver.com>
2024-05-23 23:47:04 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,31 +1,15 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
-N, M = map(int, input().split())
-
-# # 1부터 N까지의 자연수 중에서 M개를 고른 수열
-# # 같은 수 중복 가능
-# # 고른 수열은 비내림차순이어야 한다 -> 오름차순
-
-n = []
-def dfs():
-    if len(n) == M:
-        print(" ".join(map(str, n)))
+def dfs(start):
+    if len(ans) == M:
+        print(" ".join(map(str, ans)))
         return 
-    for i in range(1, N+1):
-        if promising(i):
-            dfs()
-            n.pop()
+    for i in range(start, N+1):
+        ans.append(i)
+        dfs(i)
+        ans.pop()
 
-def promising(i):
-    n.append(i)
-    a = 0
-    for i in n:
-        if a<=i:
-            a = i
-        else:
-            n.pop()
-            return False
-    return True
-
-dfs()
+N, M = map(int, input().split())
+ans = []
+dfs(1)

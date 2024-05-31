520d4437685384fa160d06ee11cd9a3c9cdc264e
박상민 <pp8817@naver.com>
2024-05-31 13:04:48 +0900

    [Silver I] Title: 트리 순회, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,33 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+N = int(input())
+tree = {}
+
+for _ in range(N):
+    root, left, right = input().split()
+    tree[root] = [left, right]
+
+def preorder(node):
+    if node != ".":
+        print(node, end='')
+        preorder(tree[node][0])
+        preorder(tree[node][1])
+
+def inorder(node):
+    if node != ".":
+        inorder(tree[node][0])
+        print(node, end='')
+        inorder(tree[node][1])
+
+def postorder(node):
+    if node != ".":
+        postorder(tree[node][0])
+        postorder(tree[node][1])
+        print(node, end='')
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')

fcd31a07f73da552f38b78d89a92d00a67c12f66
박상민 <pp8817@naver.com>
2024-08-31 22:04:57 +0900

    [level 1] Title: 모의고사, Time: 1.91 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,23 @@
+def solution(answers):
+    answer = []
+    a = [1,2,3,4,5]*2000
+    b = [2,1,2,3,2,4,2,5]*1250
+    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]*1000
+    count = [0,0,0]
+    for i, ans in enumerate(answers):
+        if a[i] == ans:
+            count[0] += 1
+        if b[i] == ans:
+            count[1] += 1
+        if c[i] == ans:
+            count[2] += 1
+    
+    m = 0
+    for i, value in enumerate(count):
+        if value > m:
+            answer = [i+1]
+            m = value
+        elif value == m:
+            answer.append(i+1)
+        
+    return answer

83c231d360502077d5ccfd1c85b6e1d0417a123c
박상민 <pp8817@naver.com>
2024-09-21 23:35:38 +0900

    [Silver I] Title: 점프, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.py
+++ 백준/Silver/1890. 점프/점프.py
@@ -9,21 +9,14 @@ dp[0][0] = 1
 for i in range(N):
     for j in range(N):
         r,c = i,j
-        if dp[r][c] == 0:
-            continue
         move = board[r][c]
-        if move == 0:
+        if dp[r][c] == 0 or move == 0:
             continue
+
         r_move, c_move = r+move, c+move
         if r+move<N:
-            if dp[r_move][c] == 0:
-                dp[r_move][c] = dp[r][c]
-            else:
                 dp[r_move][c] += dp[r][c]
         if c+move<N:
-            if dp[r][c_move] == 0:
-                dp[r][c_move] = dp[r][c]
-            else:
                 dp[r][c_move] += dp[r][c]
 
 print(dp[N-1][N-1])

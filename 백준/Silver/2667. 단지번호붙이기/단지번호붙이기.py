483861275bb5d59b5ba50cc6bb63ef28ddd1069b
박상민 <pp8817@naver.com>
2024-10-09 23:55:27 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,41 +1,43 @@
 import sys
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
 
-def dfs(x, y):
-    if is_promissing(x, y):
-        visited[x][y] = 1
-        for nx, ny in d:
-            dfs(x + nx, y + ny)
+def dfs(r,c):
+    global count
+    if promissing(r,c):
+        count += 1
+        visited[r][c] = True
+        for dx, dy in d:
+            dfs(r+dx, c+dy)   
 
-def is_promissing(row, col):
-    if 0 <= row < N and 0 <= col < N and grid[row][col] == 1 and visited[row][col] == 0:
-        global count
-        count+=1
+def promissing(r,c):
+    if 0<=r<N and 0<=c<N and graph[r][c] == 1 and not visited[r][c]:
         return True
     else:
         return False
 
 N = int(input())
-grid = [[] for _ in range(N)]
-visited = [[0] * N for _ in range(N)]
+graph = [[] for _ in range(N)]
 
 for i in range(N):
     row = input()
-    for j in range(N):
-        grid[i].append(int(row[j]))
+    for r in row:
+        graph[i].append(int(r))
 
-d = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+visited = [[False]*N for _ in range(N)]
+d = [[1,0], [-1,0], [0,1], [0,-1]]
 count = 0
-result=0
-c = []
+count_list = []
+ans = 0
+
 for i in range(N):
     for j in range(N):
-        if is_promissing(i, j):
-            dfs(i, j)
-            result+=1
-            c.append(count-1)
-            count=0
-print(result)
-c.sort()
-for i in c:
-    print(i)
+        if promissing(i,j):
+            dfs(i,j)
+            count_list.append(count)
+            count = 0
+            ans += 1
+
+count_list.sort()
+print(ans)
+for c in count_list:
+    print(c)

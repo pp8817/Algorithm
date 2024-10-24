18cf5398f47c5f9494a63aea6e3e8521eac35dc0
박상민 <pp8817@naver.com>
2024-10-24 20:15:56 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,30 +1,29 @@
 import sys
+input = lambda: sys.stdin.readline().rstrip()
 from collections import deque
- 
-n,m = map(int,sys.stdin.readline().split())
-array=[[] for _ in range(n)]
- 
-#띄어쓰기 없는 아이들 입력 받기
-for i in range(n):
-    str = sys.stdin.readline().strip()
-    for j in range(m):
-        array[i].append(int(str[j]))
- 
-#d가 갈 수 있는 범위
-d = [[1,0],[0,1],[-1,0],[0,-1]]
-#bfs
-def bfs(y,x):
-    queue = deque()
-    queue.append((y,x))
-    while queue:
-        y1,x1 = queue.popleft()
-        for dx,dy in d:
-            if 0<=y1+dy<n and 0<=x1+dx<m:
-                if array[y1+dy][x1+dx]==1:
-                    array[y1+dy][x1+dx]=array[y1][x1]+1
-                    queue.append((y1+dy,x1+dx))
- 
- 
-#(1,1)에서 처음 시작하므로
-bfs(0,0)
-print(array[n-1][m-1])
+
+N, M = map(int, input().split())
+
+graph = [[] for _ in range(N)]
+for i in range(N):
+    arr = input()
+    for a in arr:
+        graph[i].append(int(a))
+
+visited = [[0]*M for _ in range(N)]
+q = deque([(0,0)])
+visited[0][0] = 1
+
+while q:
+    r,c = q.popleft()
+
+    if (r,c) == (N-1,M-1):
+        print(visited[r][c])
+        break
+
+    for dx,dy in [[0,1],[0,-1],[1,0],[-1,0]]:
+        nx,ny = r+dx, c+dy
+        if 0<=nx<N and 0<=ny<M: 
+            if visited[nx][ny] == 0 and graph[r][c] == 1:
+                visited[nx][ny] = visited[r][c] + 1
+                q.append((nx,ny))

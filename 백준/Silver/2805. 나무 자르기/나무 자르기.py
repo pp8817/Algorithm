ee029ce76ab99b38bdad539759a4a38285aa6b09
박상민 <pp8817@naver.com>
2024-11-04 01:09:16 +0900

    [Silver II] Title: 나무 자르기, Time: 4096 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,22 +1,22 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
-
-# (1 ≤ N ≤ 1,000,000, 1 ≤ M ≤ 2,000,000,000)
 N, M = map(int, input().split())
-trees = list(map(int, input().split()))
-start, end = 1, max(trees)
+hei = list(map(int, input().split()))
 
+start, end = 1, max(hei)
 
-while start<=end:
+while start <= end:
     mid = (start+end)//2
+    height = 0
+    
+    for h in hei:
+        if h>mid:
+            height += h-mid
 
-    log = 0
-    for i in trees:
-        if i>mid:
-            log += (i-mid)
-    if log >= M:
-        start = mid + 1
+    if height >= M:
+        start = mid+1
     else:
-        end = mid-1
+        end = mid -1
+
 print(end)

a3ac6fc164da1e2a86e4daea2ef3dc1934fbe35b
박상민 <pp8817@naver.com>
2024-11-14 23:00:39 +0900

    [Bronze I] Title: 세준세비, Time: 1848 ms, Memory: 40740 KB -BaekjoonHub

---
+++ 백준/Bronze/1524. 세준세비/세준세비.py
@@ -0,0 +1,20 @@
+test_case = int(input())
+for _ in range(test_case):
+  input()
+  sejun, sebin = map(int, input().split())
+  
+  s_power = sorted(list(map(int, input().split())))
+  b_power = sorted(list(map(int, input().split())))
+
+  while len(s_power)!= 0 and len(b_power)!=0:
+    if s_power[0] < b_power[0]:
+      del s_power[0]
+    elif s_power[0] >= b_power[0]:
+      del b_power[0]
+    
+  if len(s_power) == 0:
+    print('B')
+  elif len(b_power) == 0:
+    print('S')
+  else:
+    print('C')

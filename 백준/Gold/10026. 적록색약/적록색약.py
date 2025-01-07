97bf89829fb22965f974fedd0a937426f7fd520b
박상민 <pp8817@naver.com>
2025-01-07 15:35:35 +0900

    [Gold V] Title: 적록색약, Time: 72 ms, Memory: 35044 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -2,47 +2,59 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 from collections import deque
 
-def bfs(r,c, color):
-    q = deque()
-    q.append((r,c))
-    visited[r][c] = True
+def bfs(r,c, col):
+    q = deque([(r,c)])
 
     while q:
-        x, y = q.popleft()
+        x,y = q.popleft()
 
-        for dx, dy in d:
-            nx, ny = x+dx, y+dy
+        for dx,dy in ((0,1),(0,-1),(1,0),(-1,0)):
+            rx,ry = x+dx, y+dy
+            
+            if 0<=rx<N and 0<=ry<N:
+                if not visited[rx][ry] and ll[rx][ry] == col:
+                    q.append((rx,ry))
+                    visited[rx][ry] = True
 
-            if 0<=nx<N and 0<=ny<N:
-                if not visited[nx][ny] and grid[nx][ny] == color:
-                    visited[nx][ny] = True
-                    q.append((nx, ny))
-    return grid[r][c]
+def bfs2(r,c, col):
+    q = deque([(r,c)])
+
+    while q:
+        x,y = q.popleft()
+
+        for dx,dy in ((0,1),(0,-1),(1,0),(-1,0)):
+            rx,ry = x+dx, y+dy
+            
+            if 0<=rx<N and 0<=ry<N:
+                if not visited[rx][ry] and ll2[rx][ry] == col:
+                    q.append((rx,ry))
+                    visited[rx][ry] = True
 
 N = int(input())
-grid = [list(input()) for _ in range(N)]
-d = [[1,0], [-1,0], [0,1], [0,-1]]
-
+ll = [[]*N for _ in range(N)]
+ll2 = [[]*N for _ in range(N)]
+for i in range(N):
+    st = input()
+    for j in range(N):
+        ll[i].append(st[j])
+        if st[j] == 'R':
+            ll2[i].append('G')
+        else:
+            ll2[i].append(st[j])
 
 visited = [[False]*N for _ in range(N)]
-cnt1 = 0
+ans1 = 0
 for i in range(N):
     for j in range(N):
         if not visited[i][j]:
-            color = bfs(i,j, grid[i][j])
-            cnt1+=1
+            bfs(i,j,ll[i][j])
+            ans1 += 1
 
-# 적록색약인 경우
-dic = {"R":0, "G":0}
-for i in range(N):
-    for j in range(N):
-        if grid[i][j] == 'G':
-            grid[i][j] = 'R'
-visited = [[0] * N for _ in range(N)]
-cnt2 = 0
+visited = [[False]*N for _ in range(N)]
+ans2 = 0
 for i in range(N):
     for j in range(N):
         if not visited[i][j]:
-            bfs(i,j, grid[i][j])
-            cnt2 += 1
-print(cnt1, cnt2)
+            bfs2(i,j,ll2[i][j])
+            ans2 += 1
+print(ans1, ans2)

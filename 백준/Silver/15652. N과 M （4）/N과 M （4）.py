9ea857395068daee14b9c0cdd36bc4c4b4cb721a
박상민 <pp8817@naver.com>
2025-01-11 14:18:32 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -4,12 +4,12 @@ input = lambda: sys.stdin.readline().rstrip()
 def dfs(start):
     if len(ans) == M:
         print(" ".join(map(str, ans)))
-        return 
+        return
     for i in range(start, N+1):
         ans.append(i)
         dfs(i)
         ans.pop()
-
+    
 N, M = map(int, input().split())
 ans = []
 dfs(1)

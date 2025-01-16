eb3b6d5ef5d994c140f6c6e6797d40a270ed8664
박상민 <pp8817@naver.com>
2025-01-16 17:29:23 +0900

    [Silver II] Title: 제곱수의 합, Time: 5696 ms, Memory: 35320 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -0,0 +1,18 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+import math
+
+N = int(input())
+dp = [int(1e9)]*(N+1) # 1~N의 제곱수 항 최소 개수를 표현할 리스트
+for i in range(1, int(math.sqrt(N))+1):
+    dp[i**2] = 1
+
+for i in range(2, N+1):
+    if dp[i] == 1:
+        continue
+    
+    for j in range(int(math.sqrt(i)), 0, -1):
+        dp[i] = min(dp[i],
+                1+dp[i-j**2])
+
+print(dp[N])

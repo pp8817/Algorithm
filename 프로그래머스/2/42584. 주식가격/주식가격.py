17be0ef4652d58b14bdc93d3ec9e3e319fddf06a
박상민 <pp8817@naver.com>
2025-02-12 14:01:29 +0900

    [level 2] Title: 주식가격, Time: 124.31 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -4,9 +4,9 @@ def solution(prices):
     for i in range(N):
         time = 0
         price = prices[i]
-        for j in range(i, N):
+        for j in range(i+1, N):
             time += 1
             if price > prices[j]:
                 break
-        answer[i] = time-1
+        answer[i] = time
     return answer

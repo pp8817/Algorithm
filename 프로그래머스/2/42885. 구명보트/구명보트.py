ce208e29b2fa6c7cf11ee674f043c1fd5c5aa283
박상민 <pp8817@naver.com>
2025-02-16 14:08:46 +0900

    [level 2] Title: 구명보트, Time: 9.25 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -5,16 +5,10 @@ def solution(people, limit):
     start, end = 0, len(people)-1
     
     while start <= end:
-        x1, x2 = people[start], people[end]
         answer += 1
         
-        if x1+x2 <= limit:
-            start += 1
+        if people[start]+people[end] <= limit:
             end -= 1
-        else:
-            start += 1
-        
-    if not len(people):
-        answer += 1
+        start += 1
         
     return answer

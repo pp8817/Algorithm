3a2df77c875e23f4a71f3f560be62323daca8e66
박상민 <pp8817@naver.com>
2025-02-19 01:26:49 +0900

    [Gold V] Title: 최소비용 구하기, Time: 300 ms, Memory: 58232 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -2,40 +2,33 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 import heapq
 
-INF = int(1e9)
-
-def dijkstra(start):
-    dist = [INF]*(N+1)
-    q = []
-    heapq.heappush(q, (0, start))
-    dist[start] = 0
-
-    while q:
-        current_dist, node = heapq.heappop(q)
-
-        if dist[node] < current_dist:
-            continue
-
-        for v, distance in graph[node]:
-            cost = distance + dist[node]
-
-            if dist[v] > cost:
-                heapq.heappush(q, (cost, v))
-                dist[v] = cost
-
-    return dist
-
-
-N = int(input())
-M = int(input())
+N = int(input()) # 도시의 개수
+M = int(input()) # 버스의 개수
 
 graph = [[] for _ in range(N+1)]
-
 for _ in range(M):
-    a,b,c = map(int, input().split())
-    graph[a] += [(b,c)]
+    s,e,w = map(int, input().split()) # 출발지, 도착지, 비용
+    graph[s].append((e,w))
 
-s, point = map(int, input().split())
+# 출발 지점, 도착 지점
+start_point, end_point = map(int, input().split())
 
-result = dijkstra(s)
-print(result[point])
+INF = int(1e9)
+
+distance = [INF]*(N+1)
+distance[start_point] = 0 # 출발 지점 거리 초기화
+
+q = [(0, start_point)]
+
+while q:
+    dist, now = heapq.heappop(q)
+    
+    if dist > distance[now]: # 현재 노드까지의 거리가 저장되어 있는 값보다 크다면 패스
+        continue
+
+    for e,w in graph[now]:
+        if dist+w < distance[e]:
+            distance[e] = dist+w
+            heapq.heappush(q, (dist+w, e))
+
+print(distance[end_point])

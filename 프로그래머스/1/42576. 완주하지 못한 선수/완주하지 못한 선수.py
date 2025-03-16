833830337ae70376c268b3b97668d81bfcf96975
박상민 <pp8817@naver.com>
2025-03-16 15:15:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.14 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,15 @@
 def solution(participant, completion):
-    answer = ''
     maps = {}
-    
-    for p in participant:
-        if p in maps:
-            maps[p] += 1
+    for i in participant: 
+        if i in maps:
+            maps[i] += 1
         else:
-            maps[p] = 1
+            maps[i] = 1
+    
     
     for c in completion:
         maps[c] -= 1
-
-    for key, value in maps.items():
-        if value != 0:
-            return key
+    
+    for name, v in maps.items():
+        if v:
+            return name

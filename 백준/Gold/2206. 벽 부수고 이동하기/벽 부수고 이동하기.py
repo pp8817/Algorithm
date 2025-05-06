9ce4c484d82de608b71f36919e90f99a2f4742b9
박상민 <pp8817@naver.com>
2025-05-06 14:57:07 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3768 ms, Memory: 189668 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,29 +1,42 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
 from collections import deque
 
-N, M = map(int, input().split())
-d = [[0,1], [0,-1], [1,0], [-1,0]]
-grid = [list(map(int, input())) for _ in range(N)]
-
-visited = [[[0]*2 for _ in range(M)] for _ in range(N)]
-visited[0][0][0] = 1
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 
 def bfs():
+    # visited[x][y][c]
+        # c == 0: 벽을 아직 안 깬 상태에서 (x, y)에 도달한 경우
+        # c == 1: 벽을 한 번 깨고 (x, y)에 도달한 경우
+    visited = [[[0]*2 for _ in range(M)] for _ in range(N)]
     q = deque([(0,0,0)])
+    visited[0][0][0] = 1
+
     while q:
-        x, y, k = q.popleft()
-        
+        x,y,wall = q.popleft()
+
         if (x,y) == (N-1, M-1):
-            return visited[x][y][k]
+            return visited[x][y][wall]
         
-        for dx, dy in d:
-            nx, ny = x+dx, y+dy
-            if 0<=nx<N and 0<=ny<M:
-                if grid[nx][ny] == 1 and k == 0:
-                    visited[nx][ny][1] = visited[x][y][0] +1
-                    q.append((nx,ny,1))
-                elif (not grid[nx][ny]) and (not visited[nx][ny][k]):
-                    visited[nx][ny][k] = visited[x][y][k] + 1
-                    q.append((nx,ny,k))                
+        for i in range(4):
+            rx, ry = x+dx[i], y+dy[i]
+            if 0<=rx<N and 0<=ry<M:
+                if map[rx][ry] == 0 and not visited[rx][ry][wall]:
+                    visited[rx][ry][wall] = visited[x][y][wall] + 1
+                    q.append((rx, ry, wall))
+                elif map[rx][ry] == 1 and wall == 0 and not visited[rx][ry][1]:
+                    visited[rx][ry][1] = visited[x][y][0] + 1
+                    q.append((rx, ry, 1))
     return -1
 
-print(bfs())    
+        
+N, M = map(int, input().split())
+map = [[] for _ in range(N)]
+
+for i in range(N):
+    row = input()
+    for num in row:
+        map[i].append(int(num))
+
+print(bfs())

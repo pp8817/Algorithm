ba16ab6adbbe8f7bc70051e1d06323c9c8156a5b
박상민 <pp8817@naver.com>
2025-05-14 14:52:24 +0900

    [Silver V] Title: 수들의 합 5, Time: 4596 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
@@ -0,0 +1,15 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+end, sum, count = 0, 0, 0
+
+    
+for start in range(0, n):
+    while sum < n and end < n :
+        sum += end+1
+        end += 1
+    if sum == n:
+        count += 1
+    sum -= start+1
+print(count)

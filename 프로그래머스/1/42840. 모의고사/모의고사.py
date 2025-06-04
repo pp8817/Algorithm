64eba6c48cd2df10976df93472232ed936b866d0
박상민 <pp8817@naver.com>
2025-06-04 14:59:21 +0900

    [level 1] Title: 모의고사, Time: 1.94 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,19 +1,23 @@
 def solution(answers):
     answer = []
-    a = [1,2,3,4,5]*2000
-    b = [2,1,2,3,2,4,2,5]*1250
-    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]*1000
-    count = [0,0,0]
-    for i, ans in enumerate(answers):
-        if a[i] == ans:
-            count[0] += 1
-        if b[i] == ans:
-            count[1] += 1
-        if c[i] == ans:
-            count[2] += 1
+    pattern1 = [1,2,3,4,5]*2000
+    pattern2 = [2, 1, 2, 3, 2, 4, 2, 5]*1250
+    pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]*1000
     
-    m = 0
-    for i, value in enumerate(count):
-        if value == max(count):
-            answer.append(i+1)
-    return answer
+    res = [0,0,0]
+    for i, ans in enumerate(answers):
+        if pattern1[i] == ans:
+            res[0] += 1
+        if pattern2[i] == ans:
+            res[1] += 1
+        if pattern3[i] == ans:
+            res[2] += 1
+    
+    ans = []
+    m = max(res)
+    for i, c in enumerate(res):
+        if res[i] == m:
+            ans.append(i+1)
+    
+    return ans
+    

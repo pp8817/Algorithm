b573e40cf57c05a202de23d3da1e598da905bb53
박상민 <pp8817@naver.com>
2025-06-04 14:29:30 +0900

    [level 1] Title: 최소직사각형, Time: 4.19 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,11 +1,4 @@
+# 각 명함을 돌리면서 명함을 돌려도 기존 길이 최댓값보다 작다면 통과
 def solution(sizes):
-    answer = 0
-    w,h = [], []
-    for s in sizes:
-        s.sort()
-        w.append(s[0])
-        h.append(s[1])
-        
-    answer = max(w)*max(h)
-    
-    return answer
+    rotated = [sorted(card, reverse=True) for card in sizes]
+    return max(card[0] for card in rotated) * max(card[1] for card in rotated)

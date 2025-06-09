8e30868fe914ecc9517ba495b158bd6dd19a49b6
박상민 <pp8817@naver.com>
2025-06-09 19:45:08 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.02 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.02 ms
+메모리: 9.02 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 16일 00:30:54
+2025년 06월 09일 19:45:05
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,12 +1,15 @@
 def solution(n, lost, reserve):
-    # 여벌 체육복을 가져왔지만 도난당한 학생 제외
+    # 여벌을 가진 학생이 도난을 당한 경우 제외
     lost_set = set(lost) - set(reserve)
     reserve_set = set(reserve) - set(lost)
-    
+
     for r in sorted(reserve_set):
-        if r - 1 in lost_set:
-            lost_set.remove(r - 1)
-        elif r + 1 in lost_set:
-            lost_set.remove(r + 1)
+        if r-1 in lost_set:
+            lost_set.remove(r-1)
+        elif r+1 in lost_set:
+            lost_set.remove(r+1)
     
     return n - len(lost_set)
+    
+    
+    

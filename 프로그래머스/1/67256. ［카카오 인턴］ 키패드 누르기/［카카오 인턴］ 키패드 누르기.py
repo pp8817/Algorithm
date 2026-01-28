8ce1ee5560dc651e442f6945391ffecffdf20099
박상민 <pp8817@naver.com>
2026-01-28 14:25:14 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.64 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,46 +1,41 @@
 def solution(numbers, hand):
-    answer = []
-    left_num = [1,4,7]
-    right_num = [3,6,9]
-    middle_num = [2,5,8,9]
+    maps = {1: [0,0], 2: [0,1], 3: [0,2],
+           4: [1,0],5: [1,1],6: [1,2],
+           7: [2,0],8: [2,1],9: [2,2],
+           0: [3,1],}
+            
+    left_start = (3,0)
+    right_start = (3,2)
     
-    if hand == "left":
-        hand = "L"
-    else:
-        hand = "R"
-    
-    pos = {
-    1: (0,0), 2: (0,1), 3: (0,2),
-    4: (1,0), 5: (1,1), 6: (1,2),
-    7: (2,0), 8: (2,1), 9: (2,2),
-    '*': (3,0), 0: (3,1), '#': (3,2)
-    }
-    
-    left, right = pos["*"], pos["#"]
+    answer = ""
     
     for num in numbers:
-        if num in left_num:
-            answer.append("L")
-            left = pos[num]
-        elif num in right_num:
-            answer.append("R")
-            right = pos[num]
+        if num in [1,4,7]:
+            answer += "L"
+            left_start = maps[num]
+        
+        elif num in [3,6,9]:
+            answer += "R"
+            right_start = maps[num]
         else:
-            x, y = pos[num]
-            l_dist = abs(left[0] - pos[num][0]) + abs(left[1] - pos[num][1])
-            r_dist = abs(right[0] - pos[num][0]) + abs(right[1] - pos[num][1])
+            point = maps[num]
+            left_distance = abs(point[0] - left_start[0]) + abs(point[1] - left_start[1])
+            right_distance = abs(point[0] - right_start[0]) + abs(point[1] - right_start[1])
             
-            if l_dist < r_dist:
-                answer.append("L")  
-                left = pos[num]
-            elif r_dist < l_dist:
-                answer.append("R")
-                right = pos[num]
+            if left_distance > right_distance:
+                answer += "R"
+                right_start = maps[num]
+            elif left_distance < right_distance:
+                answer += "L"
+                left_start = maps[num]
             else:
-                answer.append(hand)
-                if hand == "L":
-                    left = pos[num]
+                if hand == "left":
+                    answer += "L"
+                    left_start = maps[num]
                 else:
-                    right = pos[num]
-    
-    return "".join(answer)
+                    answer += "R"
+                    right_start = maps[num]
+    return answer
+                
+            
+            

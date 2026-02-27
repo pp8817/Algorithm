edb19356b0136969d8ba84f77166b5df381a04b6
박상민 <pp8817@naver.com>
2026-02-28 00:53:53 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,12 @@
+SELECT
+    d.ID,
+    d.EMAIL,
+    d.FIRST_NAME,
+    d.LAST_NAME
+FROM DEVELOPERS d
+WHERE (d.SKILL_CODE & (
+        SELECT SUM(CODE)
+        FROM SKILLCODES
+        WHERE CATEGORY = 'Front End'
+    )) <> 0
+ORDER BY d.ID ASC;

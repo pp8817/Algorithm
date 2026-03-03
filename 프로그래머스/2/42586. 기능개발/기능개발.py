54855a06d40cb0ad4ac6c12a6dd5c8e30d7dbc60
박상민 <pp8817@naver.com>
2026-03-03 18:18:12 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,18 +1,20 @@
 def solution(progresses, speeds):
+    days = []
+    for p, s in zip(progresses, speeds):
+        days.append(((100-p)+s-1)//s)
+    
+    cnt = 1
     answer = []
-    idx = 0
-    l = len(progresses)
+    current = days[0]
     
-    while idx < l:
-        for i in range(l):
-            progresses[i] += speeds[i]
-            
-        temp = 0
-        while idx < l and progresses[idx] >= 100:
-            idx += 1
-            temp += 1
-            
-        if temp > 0:
-            answer.append(temp)
-    
+    for d in days[1:]:
+        if current >= d:
+            cnt += 1
+        else:
+            answer.append(cnt)
+            cnt = 1
+            current = d
+    answer.append(cnt)
     return answer
+    
+        

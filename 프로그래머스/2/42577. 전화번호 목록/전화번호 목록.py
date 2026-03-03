2c3f3896b600de483b4569ff9df262e588598143
박상민 <pp8817@naver.com>
2026-03-03 16:17:00 +0900

    [level 2] Title: 전화번호 목록, Time: 95.37 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,8 @@
 def solution(phone_book):
     phone_book.sort()
     N = len(phone_book)
-    
-    for i in range(N-1):
-        if phone_book[i+1].startswith(phone_book[i]):
+    for i in range(1, N):
+        if phone_book[i].startswith(phone_book[i-1]):
             return False
-    return True
     
+    return True

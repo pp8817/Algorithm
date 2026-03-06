11656597d716be7853fb551c6b1f79d68404d58a
박상민 <pp8817@naver.com>
2026-03-06 23:46:39 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,14 +1,11 @@
 def solution(n, lost, reserve):
-    lost_set = set(lost) - set(reserve)
-    reserve_set = set(reserve) - set(lost)
+    set_lost = set(lost) - set(reserve) # 여벌 체육복을 자신이 사용하는 경우 제외
+    set_reserve = set(reserve) - set(lost) # 여벌 체육복을 자신이 사용하는 경우 제외
 
-    for r in sorted(reserve_set):
-        if r-1 in lost_set:
-            lost_set.remove(r-1)
-        elif r+1 in lost_set:
-            lost_set.remove(r+1)
-    
-    return n - len(lost_set)
-    
-    
-    
+    for r in sorted(set_reserve):
+        if r-1 in set_lost:
+            set_lost.remove(r-1)
+        elif r+1 in set_lost:
+            set_lost.remove(r+1)
+    return n-len(set_lost)
+            
